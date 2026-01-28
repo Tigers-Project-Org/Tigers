8521e658077f8b1dcdba4ef3ea804661fff7e26d
Clementine00 <93353231+Clementine00@users.noreply.github.com>
2026-01-27 19:06:39 -0700

    added shape class

---
+++ app/src/main/java/com/example/tigers/Shape.java
@@ -0,0 +1,17 @@
+package com.example.tigers;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+}
